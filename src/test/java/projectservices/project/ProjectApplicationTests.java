9297c46bc6a65b73d64305e174497859d4fdd46f
cesarinobruno <bruno.rjsantos@hotmail.com>
2021-06-30 22:28:26 -0300

    Primeiro commit - criação back-end

---
+++ src/test/java/projectservices/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package projectservices.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
