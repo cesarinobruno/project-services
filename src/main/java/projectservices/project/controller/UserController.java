448b54deca85694b842947dbc8dd49507da1ae54
cesarinobruno <bruno.rjsantos@hotmail.com>
2021-07-02 22:57:01 -0300

    Implementação busca por todos os usuários, e reordenação de usuários

--- src/main/java/projectservices/project/controller/UserController.java
+++ src/main/java/projectservices/project/controller/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 import projectservices.project.model.User;
 import projectservices.project.repository.UserRepository;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @RestController
 public class UserController {
 
@@ -13,10 +16,32 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping(path = "/api/user/{id}")
-    public ResponseEntity oneUser(@PathVariable("id") Integer id) {
+    public ResponseEntity oneUser(@PathVariable("id") Integer id)
+    {
         return userRepository.findById(id)
                 .map(u -> ResponseEntity.ok().body(u))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @RequestMapping("/api/user")
+    public List<User> allUser()
+    {
+        List<User> listUser = (List) userRepository.findAll();
+        return listUser.size() > 2 ? listUser : null;
+    }
+
+    @GetMapping("/api/user/reorder")
+    public List<User> reorder()
+    {
+        List<User> listUser = (List)userRepository.findAll();
+        Collections.sort(listUser, new Comparator<User>() {
+            @Override
+            public int compare(User u1, User u2)
+            {
+               return u1.getName().compareTo(u2.getName());
+            }
+        });
+        return listUser;
     }
 
     @PostMapping(path = "/api/user/save")
